cf608135dba0794698068ac7afc17e3525939316
Jorge <jorge.velarde0422@gmail.com>
2023-03-24 04:13:37 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(Hello) {
+    return Hello.toUpperCase();
+}
+
+function whisper(HELLO) {
+    return HELLO.toLowerCase();
+}
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string) {
+    if (string === "hello")
+    return "I can\'t hear you!";
+    else if (string === "HELLO")
+    return "YES INDEED!";
+    else if (string === "Let\'s have dinner together!")
+    return "I would love to!"
+}
+
+
+
+
+
+
+
